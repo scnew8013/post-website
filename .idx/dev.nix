44b13bac41f926b77fa66db4b09469a038d03250
scnew8013 <scnew8013@gmail.com>
2025-09-17 23:29:27 +0000

    mmmmmm

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,7 +2,7 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "stable-25.05"; # or "unstable"
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
@@ -13,6 +13,8 @@
     # pkgs.nodePackages.nodemon
     pkgs.graphite-cli
     pkgs.fish
+    pkgs.zed-editor
+    pkgs.btop
   ];
 
   # Sets environment variables in the workspace
